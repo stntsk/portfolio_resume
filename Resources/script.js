ffc4bcec35bcbec7034c348b15f41e88e16bec78
Sten_T <sten.tuisk@gmail.com>
2022-05-18 16:03:39 +0300

    pilot almost done, developer to go

--- Resources/script.js
+++ Resources/script.js
@@ -1,1 +1,21 @@
 // switching mains
+
+let pilotButton = document.getElementById('pilot_button');
+let devButton = document.getElementById('dev_button');
+let developer = document.getElementById('developer');
+let pilot =  document.getElementById('pilot');
+
+
+const switchMain = () => {
+    developer.style.display = 'block';
+    pilot.style.display= 'none';
+};
+
+pilotButton.onclick = switchMain;
+
+let switchBack = () => {
+    pilot.style.display = 'block';
+    developer.style.display = 'none';
+};
+
+devButton.onclick = switchBack;
