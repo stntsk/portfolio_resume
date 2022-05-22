5ae952ca9e4fe6a045b5c159ba8b672c29985eab
Sten_T <sten.tuisk@gmail.com>
2022-05-22 17:00:50 +0300

    Resume v1.0 page and mobile view ready

--- Resources/script.js
+++ Resources/script.js
@@ -4,11 +4,15 @@ let pilotButton = document.getElementById('pilot_button');
 let devButton = document.getElementById('dev_button');
 let developer = document.getElementById('developer');
 let pilot =  document.getElementById('pilot');
+let pilotNav = document.getElementById('pilot_nav');
+let devNav = document.getElementById('dev_nav');
 
 
 const switchMain = () => {
     developer.style.display = 'block';
-    pilot.style.display= 'none';
+    pilot.style.display = 'none';
+    devNav.style.display = 'block';
+    pilotNav.style.display = 'none';
 };
 
 pilotButton.onclick = switchMain;
@@ -16,6 +20,8 @@ pilotButton.onclick = switchMain;
 let switchBack = () => {
     pilot.style.display = 'block';
     developer.style.display = 'none';
+    pilotNav.style.display = 'block';
+    devNav.style.display = 'none';
 };
 
 devButton.onclick = switchBack;
